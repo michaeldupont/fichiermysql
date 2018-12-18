0e065527926a210053fafdb95abdf32644ad468f
Unknown <michael@homedupont.fr>
2018-12-18 11:17:55 +0100

    n

--- db.py
+++ db.py
@@ -7,8 +7,6 @@ Created on Tue Dec 18 10:34:09 2018
 
 import _mysql 
 
-db=_mysql.connect()
-
 db=_mysql.connect(host="localhost",user="chnordfr_zenc402", passwd="Ezwof01d!",db="chnord_python")
 
 db.query("""SELECT * FROM `test` WHERE 1""")
