bfa100bf5e4d6ea59d807dafbb66e65ae18a69be
Unknown <michael@homedupont.fr>
2018-12-19 10:36:59 +0100

    c

--- db.py
+++ db.py
@@ -5,9 +5,21 @@ Created on Tue Dec 18 10:34:09 2018
 @author: fmdu
 """
 
-import _mysql 
+import _mysql
+import os
 
-db=_mysql.connect(host="localhost",user="chnordfr_zenc402", passwd="Ezwof01d!",db="chnord_python")
+############ ouverture de fichier et lecture
+
+with open('data_test.txt', 'r') as mon_fichier:
+    texte = mon_fichier.read()
+    print (text)
+
+
+############ gestion de la base de données mysql : écriture du fichier en base
+
+
+
+db=_mysql.connect(host="localhost",user="chnordfr_mike", passwd="Ezwof01d!",db="chnordfr_python")
 
 db.query("""SELECT * FROM `test` WHERE 1""")
 
