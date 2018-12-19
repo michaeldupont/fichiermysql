cc990dcfbce25317df52bc77fef52d9ed0e7fee5
Unknown <michael@homedupont.fr>
2018-12-19 18:17:39 +0100

    pas sur

--- db.py
+++ db.py
@@ -22,9 +22,15 @@ with open('data_test.txt', 'r') as mon_fichier:
             print(words)
             print(words[i])
         db=_mysql.connect(host="localhost",user="chnordfr_mike", passwd="Ezwof01d!",db="chnordfr_python")
-        db.query("""INSERT INTO `test`(`number`, `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) 
-        VALUES (NULL,words[0],words[1],words[2],words[3],
-                 words[4],words[5],words[6],words[7],words[8],words[9],words[10]) """)
+        
+        requete = 
+        data = 
+        
+        db.query("""INSERT INTO `test`(`number`, \
+        `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) \
+        VALUES (NULL,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')""" % (words[0], \
+        words[1],words[2],words[3], words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
+        
         r=db.store_result()
         print("ligne",r)
 
