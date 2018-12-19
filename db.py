7220e94be7d176360a98496ce1d318e0e25e7102
Unknown <michael@homedupont.fr>
2018-12-19 19:13:28 +0100

    mariadb driver

--- db.py
+++ db.py
@@ -6,7 +6,9 @@ Created on Tue Dec 18 10:34:09 2018
 """
 
 import os
-import _mysql
+import mysql.connector as mariadb
+
+
 
 
 ############ ouverture de fichier et lecture
@@ -21,16 +23,14 @@ with open('data_test.txt', 'r') as mon_fichier:
         for i in range(0,len(words)-1):
             print(words)
             print(words[i])
-        db=_mysql.connect(host="localhost",user="chnordfr_mike", passwd="Ezwof01d!",db="chnordfr_python")
         
-        c=db.cursor()
-        c.execute("""INSERT INTO `test`(`number`, \
+        mariadb_connection = mariadb.connect(user='chnordfr_mike', password='Ezwof01d!', database='chnordfr_python')
+        cursor = mariadb_connection.cursor()
+        cursor.execute("INSERT INTO `test`(`number`, \
         `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) \
-        VALUES (NULL,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')""", (words[0], 
-        words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
-        
-        r=db.store_result()
-        print("ligne",r)
+        VALUES (NULL,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')", (words[0], words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
+        mariadb_connection.commit()       
+        print("ligne",mariadb_connection.insert_id())
 
 ############ gestion de la base de données mysql : écriture du fichier en base
 
