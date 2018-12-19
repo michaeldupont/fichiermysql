48a12eb8bfec77afcd407728d349e97413650a6f
Unknown <michael@homedupont.fr>
2018-12-19 19:53:29 +0100

    w

--- db1.py
+++ db1.py
@@ -9,7 +9,15 @@ import os
 import mysql.connector as mariadb
 
 
-
+def inputdb(line):
+    words = line.split(";")
+    mariadb_connection = mariadb.connect(user='chnordfr_mike', password='Ezwof01d!', database='chnordfr_python')
+    cursor = mariadb_connection.cursor()
+    cursor.execute("INSERT INTO `test`(`number`, \
+        `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) \
+        VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (words[0], words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
+    mariadb_connection.commit()
+    mariadb_connection.close()
 
 ############ ouverture de fichier et lecture
 
@@ -19,15 +27,6 @@ with open('data_test.txt', 'r') as mon_fichier:
         ligne=mon_fichier.readline()
         if ligne =="":
             break
-        inputdb (ligne)
+        inputdb(ligne)
 
 
-def inputdb (line):
-    words = line.split(";")
-    mariadb_connection = mariadb.connect(user='chnordfr_mike', password='Ezwof01d!', database='chnordfr_python')
-    cursor = mariadb_connection.cursor()
-    cursor.execute("INSERT INTO `test`(`number`, \
-        `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) \
-        VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (words[0], words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
-    mariadb_connection.commit()
-    mariadb_connection.close()
